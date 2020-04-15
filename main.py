85ba690e54ec9d649221b3af545a161d4b46cf25
orlovkn <orlovkn.job@gmail.com>
2020-04-15 07:02:43 +0300

    wip

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hi")
+import time
+from datetime import datetime as dt
+
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+website_list = ["facebook.com", "www.facebook.com", "vk.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day,9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,18):
+        print("Working hours...")
+    else:
+        print("Fun hours...")
+    time.sleep(5)
